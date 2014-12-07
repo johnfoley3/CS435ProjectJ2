2cb3d4e680b998e553e02da71b56bce509bd982d
John Foley <johnfoley3@gmail.com>
2014-12-06 21:23:29 -0600

    initial commit

---
+++ src/FoleyRL.java
@@ -0,0 +1,69 @@
+/**
+ * Created by John Foley on 12/6/14.
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FoleyRL {
+
+  /**
+   * Main function that's in charge of creating and running the threads
+   *
+   * @param args should be a file name containing input
+   */
+  public static void main(String[] args) {
+
+    String filename = args[0];
+
+    // We don't need numThreads, since it's the same as rows,
+    // but for simplicity sake I'm going to rename it
+    int rows = 0;
+    int cols = 0;
+    int numThreads = 0;
+
+    ArrayList<ArrayList<Integer>> pixels = new ArrayList<ArrayList<Integer>>();
+    ArrayList<ArrayList<ImplicitlyLockingIndices>> regions = new ArrayList<ArrayList<ImplicitlyLockingIndices>>();
+
+    try {
+
+      Scanner reader = new Scanner(new File(filename));
+
+      rows = reader.nextInt();
+
+      cols = reader.nextInt();
+
+      // Loop through each row
+      for (int row = 0; row < rows; row++) {
+
+        // Every row gets a temp arraylist
+        ArrayList<Integer> line = new ArrayList<Integer>();
+
+        // Read each column value into the row temp list
+        for (int col = 0; col < cols; col++) {
+
+          line.add(reader.nextInt());
+        }
+
+        // Give the row of ints to the pixel two dimensional array
+        pixels.add(line);
+      }
+
+      // Potential memory leak; always close your files!
+      reader.close();
+    } catch (FileNotFoundException e) {
+
+      System.out.println("File is not there ya dummy!");
+      System.exit(0);
+    }
+
+    numThreads = rows;
+
+    for (int i = 0; i < rows; i++) {
+
+      System.out.println(pixels.get(i));
+    }
+  }
+}
---
+++ src/ImplicitlyLockingIndices.java
@@ -0,0 +1,53 @@
+/**
+ * Created by johnfoley on 12/6/14.
+ *
+ * This class represents an index that can be read from and set to atomically
+ */
+public class ImplicitlyLockingIndices {
+
+  private int num;
+
+  /**
+   * Create a new Implicitly locking index with a default value
+   */
+  public ImplicitlyLockingIndices() {
+
+    num = 0;
+  }
+
+  /**
+   * Create a new Implicitly locking index with an initial value.
+   *
+   * @param num
+   */
+  public ImplicitlyLockingIndices(int num) {
+
+    this.num = num;
+  }
+
+  /**
+   * Atomically return the value of this index
+   *
+   * @return num
+   */
+  public int get() {
+
+    synchronized (this) {
+
+      return num;
+    }
+  }
+
+  /**
+   * Atomically set the value of this index
+   *
+   * @param value the value the index will be set to
+   */
+  public void set(int value) {
+
+    synchronized (this) {
+
+      num = value;
+    }
+  }
+}
